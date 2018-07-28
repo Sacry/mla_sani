e5a4ed5516c1002a9d2bc272d244bbb696f6d5e5
Sacry <sacry@sacry.org>
2018-07-28 17:49:29 +0900

    Add MaxPooling2D, Dropout.

--- mla_sani/nn/layers.py
+++ mla_sani/nn/layers.py
@@ -69,8 +69,8 @@ class Dense(Layer):
 
     def build(self, input_shape, optimizer=None):
         # initialize weights
-        self.kernel = np.random.randn(input_shape, self.output_shape)
-        self.bias = np.random.randn(self.output_shape)
+        self.kernel = np.random.randn(input_shape, self.output_shape) * np.sqrt(2 / self.output_shape)
+        self.bias = np.zeros(self.output_shape)
 
         super().build(input_shape, optimizer)
 
@@ -138,8 +138,16 @@ class Dummy(Activation):
 class Conv2D(Layer):
     """2D convolutional layer.
 
-    The process is almost as simple as Dense layer, except the `im2col` before forward and the
-    `col2im` after backprop.  However, the shape thing is fairly complex.
+    The process is shown as below.
+        + Dense layer
+            - forward: calculate wx + b
+            - backprop: calculate gradients -> update weights
+        + Conv2D layer
+            - forward: im2col -> calculate wx + b
+            - backprop: calculate gradients -> update weights -> col2im
+
+    It's almost the same as Dense layer except the im2col/col2im transformation.  However, the shape
+    thing is fairly complex.
 
     Args:
         filters (int): Number of filters.
@@ -177,7 +185,8 @@ class Conv2D(Layer):
 
         # initialize weights
         self.kernel = np.random.randn(self.filters, n_channels, self.kernel_size[0], self.kernel_size[1])
-        self.bias = np.random.randn(self.filters)
+        self.kernel *= np.sqrt(2 / self.kernel.size)
+        self.bias = np.zeros(self.filters)
 
         self.output_shape = self.compute_output_shape(input_shape)
         super().build(input_shape, optimizer)
@@ -229,15 +238,21 @@ class Conv2D(Layer):
 
     def compute_output_shape(self, input_shape):
         n_rows, n_cols, n_channels = input_shape
-        n_new_rows = self._compute_new_length(n_rows, self.kernel_size[0], self.strides[0], self.padding)
-        n_new_cols = self._compute_new_length(n_cols, self.kernel_size[1], self.strides[1], self.padding)
+
+        # n_new_rows = self._compute_new_length(n_rows, self.kernel_size[0], self.strides[0], self.padding)
+        # n_new_cols = self._compute_new_length(n_cols, self.kernel_size[1], self.strides[1], self.padding)
+
+        pr1, pr2, pc1, pc2 = get_pad_width(self.kernel_size, self.padding)
+        n_new_rows = (n_rows - self.kernel_size[0] + pr1 + pr2) // self.strides[0] + 1
+        n_new_cols = (n_cols - self.kernel_size[1] + pc1 + pc2) // self.strides[1] + 1
+
         return (n_new_rows, n_new_cols, self.filters)
 
     def _compute_new_length(self, length, kernel_length, stride, padding):
         if padding == 'same':
             length = length + kernel_length - 1
 
-        # new length is the length of range(start, stop, step)
+        # new length is the length of `range(start, stop, step)`
         start, stop, step = 0, length - kernel_length + 1, stride
         return (stop - start) // step + ((stop - start) % step != 0)
 
@@ -287,7 +302,7 @@ def col2im(col, image_shape, kernel_size, strides, padding='same'):
     '''
     n_samples, n_rows, n_cols, n_channels = image_shape
 
-    # initialze (padded) image
+    # initialize (padded) image
     pr1, pr2, pc1, pc2 = get_pad_width(kernel_size, padding)
     image = np.zeros((n_samples, n_rows + pr1 + pr2, n_cols + pc1 + pc2, n_channels))
 
@@ -330,9 +345,14 @@ def get_pad_width(kernel_size, padding='same'):
 def get_fancy_indices(image_size, kernel_size, strides):
     # The generated fancy indices have shape (n_new_rows, n_new_cols, kernel_size[0], kernel_size[1]).
     # It could be used to solve 'extract contiguous block from matrix' problem.
+
+    # local index within block
     indices = np.indices(kernel_size)
+    # offset along row
     i = np.arange(0, image_size[0] - kernel_size[0] + 1, strides[0])
+    # offset along column
     j = np.arange(0, image_size[1] - kernel_size[1] + 1, strides[1])
+    # offset along row and column
     ii, jj = np.meshgrid(i, j, indexing='ij')
 
     return indices[0] + ii[..., np.newaxis, np.newaxis], indices[1] + jj[..., np.newaxis, np.newaxis]
@@ -351,3 +371,144 @@ class Flatten(Layer):
 
     def backprop(self, grad):
         return grad.reshape(-1, *np.atleast_1d(self.input_shape))
+
+
+class MaxPooling2D(Layer):
+    """2D max pooling layer.
+
+    Args:
+        pool_size (tuple[int, int]): Height and width of pooling window.
+        strides (optional(tuple[int, int])): Stride along the height and width.
+    """
+    def __init__(self, pool_size=(2, 2), strides=None):
+        super().__init__()
+
+        self.pool_size = pool_size
+
+        # strides defaults to pool_size
+        if strides is None:
+            self.strides = pool_size
+        else:
+            self.strides = strides
+
+        # from cs231n:
+        # > Note that it is not common to use zero-padding for Pooling layers
+        self.padding = 'valid'
+
+        self.col = None
+
+        # keep track of the index of max value with each pool for back propagation
+        # shape = (n_channels * n_new_rows * n_new_cols * n_samples,)
+        self.argmax = None
+
+    def build(self, input_shape, optimizer=None):
+        self.output_shape = self.compute_output_shape(input_shape)
+
+        super().build(input_shape, optimizer)
+
+    def forward(self, X, train=True):
+        # |---|---|---|---|
+        # | 1 | 1 | 2 | 4 |      |---|---|
+        # | 5 | 6 | 7 | 8 |      | 6 | 8 |
+        # |---|---|---|---|  ->  |---|---|
+        # | 3 | 2 | 1 | 0 |      | 3 | 4 |
+        # | 1 | 2 | 3 | 4 |      |---|---|
+        # |---|---|---|---|
+
+        if train:
+            self.layer_input = X
+
+        n_new_rows, n_new_cols, n_channels = self.output_shape
+
+        # (n_channels * pool_size[0] * pool_size[1], n_new_rows * n_new_cols * n_samples)
+        self.col = im2col(X, self.pool_size, self.strides, self.padding)
+
+        # (n_channels * pool_size[0] * pool_size[1], n_new_rows * n_new_cols * n_samples)
+        #   -> (pool_size[0] * pool_size[1], n_channels * n_new_rows * n_new_cols * n_samples)
+        layer_output = self.col.reshape(n_channels, self.pool_size[0] * self.pool_size[1], -1)
+        layer_output = layer_output.transpose(1, 0, 2)
+        layer_output = layer_output.reshape(self.pool_size[0] * self.pool_size[1], -1)
+
+        # get index of max value within each pool
+        self.argmax = layer_output.argmax(axis=0)
+
+        # (n_channels * n_new_rows * n_new_cols * n_samples,)
+        layer_output = layer_output[self.argmax, np.arange(self.argmax.size)]
+
+        # (n_channels * n_new_rows * n_new_cols * n_samples,)
+        #   -> (n_samples, n_new_rows, n_new_cols, n_channels)
+        layer_output = layer_output.reshape(n_channels, n_new_rows, n_new_cols, -1)
+        layer_output = layer_output.transpose(3, 1, 2, 0)
+
+        return layer_output
+
+    def backprop(self, grad):
+        #                |---|---|---|---|
+        # |---|---|      | 0 | 0 | 0 | 0 |
+        # | 6 | 8 |      | 0 | 6 | 0 | 8 |
+        # |---|---|  ->  |---|---|---|---|
+        # | 3 | 4 |      | 3 | 0 | 0 | 0 |
+        # |---|---|      | 0 | 0 | 0 | 4 |
+        #                |---|---|---|---|
+        n_new_rows, n_new_cols, n_channels = self.output_shape
+
+        # (n_samples, n_new_rows, n_new_cols, n_channels)
+        #   -> (n_channels * n_new_rows * n_new_cols * n_samples,)
+        grad = grad.transpose(3, 1, 2, 0).ravel()
+
+        # (pool_size[0] * pool_size[1], n_channels * n_new_rows * n_new_cols * n_samples)
+        col_grad = np.zeros((self.pool_size[0] * self.pool_size[1], grad.size))
+        col_grad[self.argmax, np.arange(self.argmax.size)] = grad
+
+        # (pool_size[0] * pool_size[1], n_channels * n_new_rows * n_new_cols * n_samples)
+        #   -> (n_channels * pool_size[0] * pool_size[1], n_new_rows * n_new_cols * n_samples)
+        col_grad = col_grad.reshape(self.pool_size[0] * self.pool_size[1], n_channels, -1)
+        col_grad = col_grad.transpose(1, 0, 2)
+        col_grad = col_grad.reshape(n_channels * self.pool_size[0] * self.pool_size[1], -1)
+
+        return col2im(col_grad, self.layer_input.shape, self.pool_size, self.strides, self.padding)
+
+    def compute_output_shape(self, input_shape):
+        n_rows, n_cols, n_channels = input_shape
+
+        n_new_rows = (n_rows - self.pool_size[0]) // self.strides[0] + 1
+        n_new_cols = (n_cols - self.pool_size[1]) // self.strides[1] + 1
+
+        return (n_new_rows, n_new_cols, n_channels)
+
+
+class Dropout(Layer):
+    """Dropout layer.
+
+    Args:
+        rate (float): Fraction of the input units to drop.
+    """
+    def __init__(self, rate):
+        super().__init__()
+
+        self.rate = rate
+
+        self.p = None  # drop threshold
+        self.drop_mask = None
+
+    def build(self, input_shape, optimizer=None):
+        self.output_shape = input_shape
+        super().build(input_shape, optimizer)
+
+    def forward(self, X, train=True):
+        if not train:
+            return X * self.p
+
+        self.layer_input = X
+
+        # numbers of input units to drop
+        drop_units = int(np.prod(X.shape) * self.rate)
+        probs = np.random.rand(*X.shape)
+        self.p = np.partition(probs.ravel(), drop_units)[drop_units]
+        self.drop_mask = probs < self.p
+
+        return X * (~self.drop_mask)
+
+
+    def backprop(self, grad):
+        return grad * (~self.drop_mask)
